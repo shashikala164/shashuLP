75e8455c5f80301e9241ec651182f7b1c60842bf
shashikala164 <120574652+shashikala164@users.noreply.github.com>
2023-05-26 00:56:07 +0530

    LLB.py

---
+++ LLB.py
@@ -0,0 +1,32 @@
+def binary_search(arr,target):
+    low=0
+    high=len(arr)-1
+    
+    while(low<=high):
+        mid=(low+high)//2
+        
+        if arr[mid]==target:
+            return mid
+        
+        
+        elif arr[mid]<target:
+            low=mid+1
+        else:
+            high=mid-1
+            
+            
+            #example
+            
+            arr=input("enter the sorted array").split()
+            arr=[int(num) for num in arr
+            target=int(input("enter the sorted number"))
+            result=binary_search(arr,target)
+                 
+if result!= -1:
+                 print("element found")
+else:
+                 print("element not found")
+                 
+                             
+     
+  
